7d6ac07c68aa317d46f5c22eff01f67192d09887
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-08 22:46:06 +0800

    added caching for static files

--- game2048/routes.py
+++ game2048/routes.py
@@ -1,11 +1,11 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, make_response, send_from_directory
 from flask_mail import Message
 from game2048 import app, db, mail, socketio
 from game2048.forms import RegistrationForm, LoginForm
 
 # Our home is also the login page
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def home():
     # we need to check if user has logged in already to render a logged in homepage
     form = LoginForm()
@@ -30,3 +30,12 @@ def index():
     msg.body = request.args.get('message')
     mail.send(msg)
     return f"<p>Message sent!</p> {msg}"
+
+# Cache static files on client for 1 month
+# Source: https://stackoverflow.com/questions/77569410/flask-possible-to-cache-images
+# Source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Cache-Control
+@app.route('/static/<path:filename>')
+def static(filename):
+    resp = make_response(send_from_directory('static/', filename))
+    resp.headers['Cache-Control'] = 'max-age=604800'
+    return resp
