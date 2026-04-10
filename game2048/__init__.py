c7d41de15f2a342e4a9b80aba11b1801d766e403
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-10 13:32:09 +0800

    add flask migrate to code and requirement

--- game2048/__init__.py
+++ game2048/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_socketio import SocketIO
 from config import Config
@@ -11,6 +12,7 @@ app.config.from_object(Config)
 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 mail = Mail(app)
 socketio = SocketIO(app)
 
