eb715150f5b14c707c1ddd4b73de664696c846de
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-10 16:09:27 +0800

    add flask login extension to init

--- game2048/__init__.py
+++ game2048/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_socketio import SocketIO
+from flask_login import LoginManager
 from config import Config
 
 # Required for caching (static_folder)
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 mail = Mail(app)
 socketio = SocketIO(app)
 
