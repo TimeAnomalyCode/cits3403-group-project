7f099483ffd3cfca67a8a723268a449fcb768a41
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-10 14:02:36 +0800

    added flaskenv to use more of flask features and update run.py to make use of flask features

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
-from game2048 import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from game2048 import app, db
+from game2048.models import User
 
 # Use Ctrl + Shift + R to hard refresh browser
+
+# using 'flask shell' will load all the context you need
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User}
+
+# using default vscode debugger to step through programs
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
