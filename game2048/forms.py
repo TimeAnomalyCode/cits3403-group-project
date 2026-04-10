32d0fdc90aa035408014eac0a6d00d6f30b41d2b
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-10 17:48:28 +0800

    add new validation for username and password

--- game2048/forms.py
+++ game2048/forms.py
@@ -1,6 +1,9 @@
+from game2048 import db
+from game2048.models import User
+import sqlalchemy as sa
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Regexp, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Regexp, Email, EqualTo, ValidationError
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -31,6 +34,20 @@ class RegistrationForm(FlaskForm):
                              ])
     
     submit = SubmitField('Sign Up')
+
+    # Custom username validator to make sure no 2 usernames are same
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    # Custom email validator to make sure no 2 emails are the same
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 
 class LoginForm(FlaskForm):    
     email = StringField('Email', 
