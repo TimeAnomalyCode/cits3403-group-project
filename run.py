2c276b924fa166837f63ecd97e61bbeb2176da73
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-04-10 15:01:49 +0800

    remove not required variables since SQLAlchemy passes it automatically

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from game2048 import app, db
-from game2048.models import User
+from game2048 import app
 
 # Use Ctrl + Shift + R to hard refresh browser
 
 # using 'flask shell' will load all the context you need
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User}
+    return {'sa': sa, 'so': so}
 
-# using default vscode debugger to step through programs
+# using default vscode debugger to step through each line
 if __name__ == "__main__":
     app.run()
