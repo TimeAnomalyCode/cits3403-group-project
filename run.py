ed2b8cd95bcb5df0b963d33eb454072a3027c325
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-05-09 11:24:59 +0800

    no need debug=true because it's already in flaskenv

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+
 # from game2048 import app
 from game2048 import create_app, db, socketio
 from game2048.models import User, Match
@@ -7,12 +8,13 @@ from game2048.models import User, Match
 # Use Ctrl + Shift + R to hard refresh browser
 app = create_app()
 
+
 # using 'flask shell' will load all the context you need
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Match": Match}
+    return {"sa": sa, "so": so, "db": db, "User": User, "Match": Match}
 
 
 # using default vscode debugger to step through each line
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app)
